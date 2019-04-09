4181bbe41397c10319c987f535a382d028a4ce63
eric-klimowich <eklimowich@hotmail.com>
2019-04-09 17:31:04 -0400

    Added initial actions to controllers

--- app/controllers/api/v1/boxes_controller.rb
+++ app/controllers/api/v1/boxes_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::BoxesController < ApplicationController
+  before_action :find_box, only: [:update]
+
+  def index
+    @boxes = Box.all
+    render json: @boxes
+  end
+
+  def create
+    @box = Box.create(box_params)
+    if @box.valid?
+      render json: @box, status: :accepted
+    else
+      render json: { errors: @box.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    if @box.update(box_params)
+      render json: @box, status: :accepted
+    else
+      render json: { errors: @box.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    @box.destroy
+  end
+
+  private
+
+  def box_params
+    params.permit(:length, :width, :height)
+  end
+
+  def find_box
+    @box = Box.find(params[:id])
+  end
 end
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :find_item, only: [:update]
+
+  def index
+    @items = Item.all
+    render json: @items
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.valid?
+      render json: @item, status: :accepted
+    else
+      render json: { errors: @item.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: @item, status: :accepted
+    else
+      render json: { errors: @item.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+  private
+
+  def item_params
+    params.permit(:description, :quantity, :box_id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
